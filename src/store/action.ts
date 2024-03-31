7bfeaff986052b76c5b955003684284570620471
Danil Dediaev <dediaev.danil130702@gmail.com>
2024-03-31 17:41:06 +0300

    module7-task1

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,8 +1,13 @@
 import { createAction } from '@reduxjs/toolkit';
 import { City } from '../types/offer';
 
+export const setOffersDataLoadingStatus = createAction<boolean>(
+  'setOffersDataLoadingStatus'
+);
+export const loadOffers = createAction<Offer[]>('loadOffers');
+export const setError = createAction<string | null>('setError');
+
 export const changeCity = createAction<City>('changeCity');
-export const setOffersList = createAction('setOffersList');
 export const setSortType = createAction<string>('setSortType');
 export const setSelectedPoint = createAction<{ title: string } | null>(
   'setSelectedPoint'
